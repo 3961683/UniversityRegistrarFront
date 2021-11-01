a12e1e165a1a86f5bb9c0ad10c94005a6de044f5
Insar <3961683@mail.ru>
2021-11-01 15:17:06 +0600

    Start Project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="p-4 border w-100 mb-4" id="user">
+
+
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+
+
+    }
+  },
+  methods: {
+    async createUser() {
+      const user = { Name : this.user_name, Country: this.Country, Email: this.email, Password: this.password };
+      console.log(user);
+      await axios.post("https://localhost:44342/api/User", user);
+    }
+  },
+  mounted() {
+  }
+
+}
+</script>
+
+<style>
+*{
+  padding: 4px;
+}
+
+.border{
+  border: solid black 1px;
+}
+</style>
